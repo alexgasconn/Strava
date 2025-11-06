a5f4f25276c0ee0cb9a3d33a7ca3211778bd90c6
agasconn <alex.gascon1999@gmail.com>
2025-11-06 08:53:32 +0100

    utils fix

--- js/utils.js
+++ js/utils.js
@@ -79,7 +79,7 @@ export function formatPace(seconds, km) {
     return `${min}:${secRest.toString().padStart(2, '0')} /km`;
 }
 
-function formatDate(date) {
+ function formatDate(date) {
     if (!(date instanceof Date)) date = new Date(date);
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0');
