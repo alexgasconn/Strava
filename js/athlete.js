8d30eff30bedac35c561f801aa2210202f10fea1
agasconn <alex.gascon1999@gmail.com>
2026-02-23 09:34:55 +0100

    error fixing2

--- js/athlete.js
+++ js/athlete.js
@@ -39,15 +39,6 @@ export function renderAthleteTab(allActivities, dateFilterFrom, dateFilterTo, sp
 
     console.log("🎽 renderAthleteTab: Athlete tab rendered");
 }
-renderMonthlyMixChart(runs);
-renderHourMatrix(runs);
-renderYearMonthMatrix(runs);
-renderMonthWeekdayMatrix(runs);
-renderMonthDayMatrix(runs);
-renderMonthHourMatrix(runs);
-renderYearHourMatrix(runs);
-renderYearWeekdayMatrix(runs);
-renderInteractiveMatrix(runs);
 
 
 
